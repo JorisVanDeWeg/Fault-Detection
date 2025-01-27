9fe34aaabad89013b3597bffc634cfa7ee5972c8
JorisVanDeWeg <64067037+JorisVanDeWeg@users.noreply.github.com>
2025-01-27 11:04:36 +0100

    Update dataframe retrieval
    First time series did not work

--- Functions/load_data.py
+++ Functions/load_data.py
@@ -1,29 +1,44 @@
 from PU_functions import *
 from CRWU_functions import *
-
-window_duration = 4
+import pandas as pd
+import time 
+window_duration = 2
 overlap = 0
 
 # Generate 
 PU = True
-CRWU = True
+CRWU = False
 
 if PU:
     signal_indices = [6]
 
+    start_time = time.time()
     pu_base_dir_list = ['C:/Users/joris/Documents/EE Master/Year 2/Thesis/Dataset/PU/Healthy',
-                        #'C:/Users/joris/Documents/EE Master/Year 2/Thesis/Dataset/PU/Real Damages',]
-                        'C:/Users/joris/Documents/EE Master/Year 2/Thesis/Dataset/PU/Real Damages without double']
-                        #'C:/Users/joris/Documents/EE Master/Year 2/Thesis/Dataset/PU/Artificially Damaged'
+                        'C:/Users/joris/Documents/EE Master/Year 2/Thesis/Dataset/PU/Real Damages']
+                        # 'C:/Users/joris/Documents/EE Master/Year 2/Thesis/Dataset/PU/Real Damages without double']
+                        # 'C:/Users/joris/Documents/EE Master/Year 2/Thesis/Dataset/PU/Artificially Damaged']
 
     pu_features_df, pu_time_series_df = load_pu_dataset(pu_base_dir_list, 
                                                     window_duration=window_duration, 
                                                     overlap=overlap, 
                                                     signal_indices=signal_indices, 
                                                     classification_type="multiclass")
+    print(f"Elapsed time for constructing dataframes: {time.time() - start_time:.6f} seconds")  # Print elapsed time
 
-    pu_features_df.to_csv('Dataframes (csv)/pu_features_df.csv', index=False)
-    pu_time_series_df.to_csv('Dataframes (csv)/pu_time_series_df.csv', index=False)
+
+    ## Time series SAVE
+    start_time = time.time()
+    pu_time_series_df.to_parquet('Dataframes/pu_time_series_df.parquet', engine='pyarrow')
+    print(f"Elapsed time for SAVING time series: {time.time() - start_time:.6f} seconds")  # Print elapsed time
+
+    ## Features SAVE (process is already fast enough)
+    pu_features_df.to_csv('Dataframes/pu_features_df.csv', index=False)
+
+    ## Time series LOAD (tester)
+    # start_time = time.time()
+    # loaded_df = pd.read_parquet('Dataframes/pu_time_series_df.parquet', engine='pyarrow')
+    # print(f"Elapsed time for LOADING time series: {time.time() - start_time:.6f} seconds")  # Print elapsed time
+
 
 if CRWU:
     crwu_base_dir_list = ['C:/Users/joris/Documents/EE Master/Year 2/Thesis/Dataset/CWRU/Data/12k_DE',
@@ -31,8 +46,8 @@ if CRWU:
 
     crwu_features_df, crwu_time_series_df = load_crwu_dataset(crwu_base_dir_list, 
                                                             window_duration=window_duration,
-                                                            overlap=0,
+                                                            overlap=overlap,
                                                             classification_type="multiclass")
     
-    crwu_features_df.to_csv('Dataframes (csv)/crwu_features_df.csv', index=False)
-    crwu_time_series_df.to_csv('Dataframes (csv)/crwu_time_series_df.csv', index=False)
+    crwu_features_df.to_csv('Dataframes/crwu_features_df.csv', index=False)
+    crwu_time_series_df.to_csv('Dataframes/crwu_time_series_df.csv', index=False)
